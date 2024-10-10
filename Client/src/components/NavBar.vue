b8f5ed7980a3c3a42e776cd50a48c1b8710121c3
latiwagner <latiphyver.b@gmail.com>
2024-10-10 18:49:23 -0400

    file based routing implemented

--- Client/src/components/NavBar.vue
+++ Client/src/components/NavBar.vue
@@ -1,21 +1,26 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { RouterLink } from 'vue-router'
+
+const isOpen = ref(false)
 </script>
 
 <template>
-<nav class="navbar" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="30" height="30" />
+<nav class="navbar is-info" role="navigation" aria-label="main navigation">
+  <div class="container">
+    <div class="navbar-brand">
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="30" height="30" />
 
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" 
+      :class="{ 'is-active': isOpen }" @click="isOpen = !isOpen">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
 
-  <div id="navbarBasicExample" class="navbar-menu">
+  <div class="navbar-menu" :class="{ 'is-active': isOpen }">
     <div class="navbar-start">
         <RouterLink to="/" class="navbar-item">Home</RouterLink>
         <RouterLink to="/about" class="navbar-item">About</RouterLink>
@@ -27,15 +32,15 @@ import { RouterLink } from 'vue-router'
         </a>
 
         <div class="navbar-dropdown">
-          <a class="navbar-item">
+          <RouterLink to="/about" class="navbar-item">
             About
-          </a>
-          <a class="navbar-item is-selected">
+          </RouterLink>
+          <RouterLink to="/jobs" class="navbar-item">
             Jobs
-          </a>
-          <a class="navbar-item">
+          </RouterLink>
+          <RouterLink to="/contact" class="navbar-item">
             Contact
-          </a>
+          </RouterLink>
           <hr class="navbar-divider">
           <a class="navbar-item">
             Report an issue
@@ -57,11 +62,15 @@ import { RouterLink } from 'vue-router'
       </div>
     </div>
   </div>
+  </div>
 </nav>
 </template>
 
 
 
 <style scoped>
-
+  .router-link-active {
+    font-weight: bold;
+    border-bottom: 2px solid blue;
+  }
 </style>
---
+++ Client/src/pages/contact/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+      <h1>
+        Contact Us
+      </h1>
+      <p>
+        text text text text
+      </p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
