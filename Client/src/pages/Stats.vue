b083d5d8c2334e63737ad46675b224e4a63298e9
latiwagner <latiphyver.b@gmail.com>
2024-10-28 01:09:11 -0400

    very simple stats page. will improve in the future

--- Client/src/pages/Stats.vue
+++ Client/src/pages/Stats.vue
@@ -1,8 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import { getAll, type User } from '@/models/users' 
+import { refUser } from '@/models/currentUser';
+
+const users = ref<User[]>([])
+users.value = getAll().data
+const activeUser = refUser()
+</script>
 
 <template>
   <main>
-    <h1 class="title">Your Stats</h1>
-    <p>This page will allow you to see your own overall exercise statistics.</p>
+      <div v-for="logged in activeUser" :key="logged.user.id">
+        <div v-for="user in users" :key="user.id">
+          <div class="hero" v-if="logged.user.username === user.username">
+            <div class="hero-body">
+              <p class="title">Lifetime Stats</p>
+              <p class="subtitle">{{ user.totalDistance }} mi. <br/>
+                {{ user.totalDuration }} min.
+              </p>
+              
+            </div>
+          </div>
+        </div>
+      </div>
   </main>
 </template>
+
+<style scoped>
+
+</style>
